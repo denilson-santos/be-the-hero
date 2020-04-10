faca520c06597c887876f0109676946341c8c280
denilson-santos <denilson.silvasantos.contato@gmail.com>
2020-04-10 15:38:23 -0300

    Adding some validations to the application backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -21,5 +22,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
